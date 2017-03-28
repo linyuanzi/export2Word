96c12c4635ce87e9f7887e5bc5ff317ae146a34b
yuanzi <linyuanzi@honraytech.com>
2017-03-28 16:58:36 +0800

    add all new code

---
+++ Index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('WordAdapter.php');
+
+$host = 'localhost';
+$userName = 'root';
+$password = 'root';
+
+fwrite(STDOUT, 'Enter the db name:');
+$dbName = trim(fgets(STDIN));
+
+fwrite(STDOUT, 'Enter the export document name:');
+$name = trim(fgets(STDIN));
+
+try {
+	$db = new mysqli($host, $userName, $password, $dbName);
+	$db->set_charset('utf8');
+	$result = $db->query('SHOW TABLE STATUS');
+	$status = $result->fetch_all(MYSQLI_ASSOC);
+	foreach ($status as &$v) {
+		$table = $v['Name'];
+	    $result = $db->query("SHOW FULL FIELDS FROM $table");
+	    $v['fields'] = $result->fetch_all(MYSQLI_ASSOC);
+	}
+
+	$word = new \WordAdapters();
+	$word->exportWord($status, $name);
+} catch (Exception $e) {
+	die('Error:' . $e->getMessage());
+}
+
+return;
